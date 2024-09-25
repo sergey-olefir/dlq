f550a0dd3797de90034f5f55794392794f82f1bf
Serhii Olefir <senkaolefir@gmail.com>
2024-09-25 11:37:17 +0300

    master: 2024-09-25 Fix login bug
    Fix login bug causing invalid sessions
    - Modify session handling code to properly validate user credentials
    - Update error messages for clarity
    - Add new unit tests to ensure accurate login behavior

--- Dlq/TransferDeadLetterMessages.cs
+++ Dlq/TransferDeadLetterMessages.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -98,44 +99,69 @@ namespace Dlq
         {
             await this.ProcessQueueAsync(queueName, async (sender, receiver) =>
             {
-                var wait = new TimeSpan(0, 0, 10);
+                var blockingCollection = new BlockingCollection<List<ServiceBusReceivedMessage>>(boundedCapacity: 20);
+                var wait = new TimeSpan(0, 0, 30);
                 var now = DateTime.UtcNow;
 
-                this._logger.Info("starting to transfer");
-                this._logger.Info($"fetching messages ({wait.TotalSeconds} seconds retrieval timeout)");
-
-                IReadOnlyList<ServiceBusReceivedMessage> dlqMessages;
-                do
+                async Task Produce()
                 {
-                    dlqMessages = await receiver.ReceiveMessagesAsync(this._configuration.BatchSize, wait);
-                    dlqMessages = dlqMessages.Where(x => x.EnqueuedTime < now).ToArray();
+                    this._logger.Info("starting to transfer");
+                    this._logger.Info($"fetching messages ({wait.TotalSeconds} seconds retrieval timeout)");
 
-                    this._logger.Info($"dl-count: {dlqMessages.Count}");
+                    var dlqMessages = new List<ServiceBusReceivedMessage>(this._configuration.BatchSize);
+                    do
+                    {
+                        dlqMessages.Clear();
+                        IReadOnlyList<ServiceBusReceivedMessage> receivedMessages;
+                        do
+                        {
+                            receivedMessages = await receiver.ReceiveMessagesAsync(this._configuration.BatchSize, wait);
+                            dlqMessages.AddRange(receivedMessages.Where(x => x.EnqueuedTime < now).ToArray());
+                        } while (receivedMessages.Count != 0 && dlqMessages.Count < this._configuration.BatchSize);
 
-                    this._logger.Info($"dl-attempting to send batch messages: {dlqMessages.Count}");
-                    var messages = dlqMessages.Select(x => new ServiceBusMessage(x)).ToArray();
-                    await sender.SendMessagesAsync(messages);
-                    this._logger.Info($"dl-messages-sent: {dlqMessages.Count}");
+                        blockingCollection.Add(dlqMessages.ToList());
 
-                    await Task.WhenAll(dlqMessages.Select(x => this.CompleteMessageAsync(x, receiver)));
+                        this._logger.Info($"dl-count-produced: {dlqMessages.Count}");
+                    } while (dlqMessages.Count > 0);
 
-                    this._logger.Info("--------------------------------------------------------------------------------------");
-                } while (dlqMessages.Count > 0);
+                    blockingCollection.CompleteAdding();
+                    this._logger.Info("finished");
+                }
 
-                this._logger.Info("finished");
+                async Task Consume()
+                {
+                    foreach (var dlqMessages in blockingCollection.GetConsumingEnumerable())
+                    {
+                        this._logger.Info($"dl-count-consumed: {dlqMessages.Count}");
+
+                        this._logger.Info($"dl-attempting to send batch messages: {dlqMessages.Count}");
+                        var messages = dlqMessages.Select(x => new ServiceBusMessage(x)).ToArray();
+                        await sender.SendMessagesAsync(messages);
+                        this._logger.Info($"dl-messages-sent: {dlqMessages.Count}");
+
+                        await Task.WhenAll(dlqMessages.Select(x => this.CompleteMessageAsync(x, receiver)));
+
+                        this._logger.Info("--------------------------------------------------------------------------------------");
+                    }
+                }
+
+                var producerTask = Task.Run(Produce);
+
+                var tasks = Enumerable.Range(0, 5).Select(_ => Task.Run(Consume)).Append(producerTask).ToArray();
+                await Task.WhenAll(tasks);
             });
         }
 
         private async Task CompleteMessageAsync(ServiceBusReceivedMessage dlqMessage, ServiceBusReceiver dlqReceiver)
         {
-            this._logger.Info($"start completing message {dlqMessage.MessageId}");
-            this._logger.Info($"message-{dlqMessage.MessageId}-body: {dlqMessage.Body}");
-            this._logger.Info($"message-{dlqMessage.MessageId}-reason: {dlqMessage.DeadLetterReason}");
-            this._logger.Info($"message-{dlqMessage.MessageId}-error-description: {dlqMessage.DeadLetterErrorDescription}");
+            this._logger.Trace($"start completing message {dlqMessage.MessageId}");
+            this._logger.Trace($"message-{dlqMessage.MessageId}-body: {dlqMessage.Body}");
+            this._logger.Trace($"message-{dlqMessage.MessageId}-reason: {dlqMessage.DeadLetterReason}");
+            this._logger.Trace($"message-{dlqMessage.MessageId}-error-description: {dlqMessage.DeadLetterErrorDescription}");
 
             await dlqReceiver.CompleteMessageAsync(dlqMessage);
 
-            this._logger.Info($"finished completing message {dlqMessage.MessageId}");
+            this._logger.Trace($"finished completing message {dlqMessage.MessageId}");
         }
 
         private async Task ProcessTopicAsync(string topicName, string subscriberName, Func<ServiceBusSender, ServiceBusReceiver, Task> action)
